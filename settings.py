0dbfe1cd95dda689067346623b4dfe072ce8eb87
Jahangir Shabiyev <c.shabiev@gmail.com>
2016-04-30 07:16:07 +0400

    "2016-04-30T07:16:07.068563"

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG = (
-    ("CONSUMER_TOKEN", "DSKLZ2luVXAT5gxoJjAYYirB7"),
-    ("CONSUMER_SECRET", "IaBJSOzn4E4v4Vj4dtrTcaASBuD0Mu6Mj3PPjgaKHvweyhQFPM")
+    # ("CONSUMER_TOKEN", "DSKLZ2luVXAT5gxoJjAYYirB7"),
+    ("CONSUMER_TOKEN", "KeYjkDglBR0oD94Z8RhgsErUM"),
+    # ("CONSUMER_SECRET", "IaBJSOzn4E4v4Vj4dtrTcaASBuD0Mu6Mj3PPjgaKHvweyhQFPM")
+    ("CONSUMER_SECRET", "R6E9G1BLQsNiC6WvZc4XWfG54v1221tG366v75Vs2TmG5KvBuy")
 )
