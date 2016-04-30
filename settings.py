390213359cddbcbcd2c0eaebb5ad83a1816a00ea
Jahangir Shabiyev <c.shabiev@gmail.com>
2016-04-30 07:33:24 +0400

    Last fixed odun for whrs pec

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+CONFIG = (
+    ("CONSUMER_TOKEN", ""),
+    ("CONSUMER_SECRET", "")
+)
