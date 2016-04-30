1521fd96aec81f8cea27d923ddea0449ed532fb2
Jahangir Shabiyev <c.shabiev@gmail.com>
2016-04-30 07:14:26 +0400

    Complicated odun for pec

--- lat_plaza.py
+++ lat_plaza.py
@@ -3,17 +3,15 @@ import json
 import datetime
 from itertools import starmap
 from tweepy import OAuthHandler, API
+import settings
 
-CONFIG = (
-    ("CONSUMER_TOKEN", "DSKLZ2luVXAT5gxoJjAYYirB7"),
-    ("CONSUMER_SECRET", "IaBJSOzn4E4v4Vj4dtrTcaASBuD0Mu6Mj3PPjgaKHvweyhQFPM")
-)
 
-api = API(OAuthHandler(*starmap(os.environ.get, CONFIG)))
-topics = ("#plazadili", "plazadili")
+def get_statuses():
+    api = API(OAuthHandler(*starmap(os.environ.get, settings.CONFIG)))
+    topics = ("#plazadili", "plazadili")
 
-with open(datetime.datetime.now().isoformat() + ".json", "w+") as jsn:
-    json.dump(
-        {"data": [{"user": tweet.user.screen_name, "text": tweet.text} for tweet in api.search(",".join(topics))]},
-        jsn, ensure_ascii=False, indent=4
-    )
+    with open(os.path.join(settings.BASE_DIR, datetime.datetime.now().isoformat() + ".json"), "w+") as jsn:
+        json.dump(
+            {"data": [{"user": tweet.user.screen_name, "text": tweet.text} for tweet in api.search(",".join(topics))]},
+            jsn, ensure_ascii=False, indent=4
+        )
---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG = (
+    ("CONSUMER_TOKEN", "DSKLZ2luVXAT5gxoJjAYYirB7"),
+    ("CONSUMER_SECRET", "IaBJSOzn4E4v4Vj4dtrTcaASBuD0Mu6Mj3PPjgaKHvweyhQFPM")
+)
