13a2cda376870e9c6de752ea84f198f73b3d178d
Jahangir Shabiyev <c.shabiev@gmail.com>
2016-04-30 07:22:30 +0400

    Odun ready for pec

--- settings.py
+++ settings.py
@@ -4,8 +4,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 CONFIG = (
-    # ("CONSUMER_TOKEN", "DSKLZ2luVXAT5gxoJjAYYirB7"),
-    ("CONSUMER_TOKEN", "KeYjkDglBR0oD94Z8RhgsErUM"),
-    # ("CONSUMER_SECRET", "IaBJSOzn4E4v4Vj4dtrTcaASBuD0Mu6Mj3PPjgaKHvweyhQFPM")
-    ("CONSUMER_SECRET", "R6E9G1BLQsNiC6WvZc4XWfG54v1221tG366v75Vs2TmG5KvBuy")
+    ("CONSUMER_TOKEN", ""),
+    ("CONSUMER_SECRET", "")
 )
