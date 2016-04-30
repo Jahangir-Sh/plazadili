61c5d4f012b57ee9ce9aeb560e1afc37fb035478
Jahangir Shabiyev <c.shabiev@gmail.com>
2016-04-30 08:21:37 +0400

    孔子 mode on

--- plazadili.py
+++ plazadili.py
@@ -1,13 +1,30 @@
 import os
+import json
 import subprocess
 from datetime import datetime
+from itertools import starmap
+from tweepy import OAuthHandler, API
+import settings
 
-from tivit import get_statuses
-from settings import BASE_DIR
 
-get_statuses()
+def get_statuses():
+    api = API(OAuthHandler(*starmap(os.environ.get, settings.CONFIG)))
+    topics = ("#plazadili", "plazadili")
 
-os.chdir(BASE_DIR)
-subprocess.call(["git", "add", "."])
-subprocess.call("git commit -am \"{}\"".format(datetime.now().isoformat()).split())
-subprocess.call(["git", "push", "plaza", "data"])
+    with open(os.path.join(settings.DATA_DIR, datetime.now().isoformat() + ".json"), "w+") as jsn:
+        json.dump(
+            {"data": [{"user": tweet.user.screen_name, "text": tweet.text} for tweet in api.search(",".join(topics))]},
+            jsn, ensure_ascii=False, indent=4
+        )
+
+if __name__ == "__main__":
+    get_statuses()
+
+    os.chdir(settings.BASE_DIR)
+    commands = map(str.split, [
+        "git add .",
+        "git commit -am \"{}\"".format(datetime.now().isoformat()),
+        "git push plaza data"
+    ])
+
+    map(subprocess.call, commands)
