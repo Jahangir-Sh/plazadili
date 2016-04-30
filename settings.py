57a91fd7c4e84f47c73baa1d771669d58f7bacb2
Jahangir Shabiyev <c.shabiev@gmail.com>
2016-04-30 07:41:42 +0400

    "2016-04-30T07:41:42.088648"

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 CONFIG = (
-    ("CONSUMER_TOKEN", ""),
-    ("CONSUMER_SECRET", "")
+    ("CONSUMER_TOKEN", "EysrzyH0Ow1QnCHSgYjpmjoqp"),
+    ("CONSUMER_SECRET", "XhTWL1lRThmCK6MmSXkzd30nmkwl8zu21VupoQ86cvtFHSTH7B")
 )
